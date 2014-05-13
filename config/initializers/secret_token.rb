bb19eae9e017d562e8469c7ec83210a515dd6863
valerie <valerie.douglas@openlogic.com>
2014-05-13 12:36:33 -0600

    init commit 0513-12p

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewApp7::Application.config.secret_token = 'a78a6fab48858669b5ac88bf2d797285f05e0c6918599fd0440bd3a483486a2cda9b5657ca793ecf60f679eee9b16008853b945405a0ae8d75dfbcc8d6d1d52e'
