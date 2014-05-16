fbaf0c29aa3a740ea7b3b01a9f96b37cdaabcc6a
valerie <valerie.douglas@openlogic.com>
2014-05-16 17:29:45 -0600

    experiment at start of ch6

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,154 +1,83 @@
+# require 'spec_helper'
+
+# describe "Static pages" do
+
+#    subject { page }
+
+#   describe "Home page" do
+#     before { visit root_path }
+#     it { should have_selector('h1', text: 'New App7')}
+#     it { should have_selector('title', text: full_title('')) }
+#    # it { should_not have_selector 'title', text: '| Home'}
+#   end   
+
+#   describe "Help page" do
+#     before { visit help_path }
+
+#     it { should have_selector('h1',    text: 'Help') }
+#     it { should have_selector('title', text: full_title('Help')) }
+#   end
+
+#   describe "About page" do
+#     before { visit about_path }
+
+#     it { should have_selector('h1',    text: 'About') }
+#     it { should have_selector('title', text: full_title('About Us')) }
+#   end
+
+#   describe "Contact page" do
+#     before { visit contact_path }
+
+#     it { should have_selector('h1',    text: 'Contact') }
+#     it { should have_selector('title', text: full_title('Contact')) }
+#   end
+# end
+#/////////////////////////////////////////////////////////////////////
+
 require 'spec_helper'
 
 describe "Static pages" do
 
    subject { page }
 
+#below are 2 separate attempts to update above code -- both fail
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1',     text: heading) }
+    it { should have_selector('title',  text: full_title(page_title)) }
+  end
+
   describe "Home page" do
     before { visit root_path }
-    it { should have_selector('h1', text: 'New App7')}
-    it { should have_selector('title', text: full_title('')) }
-   # it { should_not have_selector 'title', text: '| Home'}
-  end   
+    let(:heading)  { 'New App7' }
+    let(:page_title) { '' }
+
+    it_should_behave_like "all static pages"
+    it { should_not have_selector 'title', text: '| Home' }
+  end
 
   describe "Help page" do
     before { visit help_path }
+    let(:heading)  { 'New App7' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1',    text: 'Help') }
-    it { should have_selector('title', text: full_title('Help')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
     before { visit about_path }
+    let(:heading)  { 'New App7' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1',    text: 'About') }
-    it { should have_selector('title', text: full_title('About Us')) }
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
+    let(:heading)  { 'New App7' }
+    let(:page_title) { '' }
 
-    it { should have_selector('h1',    text: 'Contact') }
-    it { should have_selector('title', text: full_title('Contact')) }
+    it_should_behave_like "all static pages"
   end
+
 end
-
-# below are 2 separate attempts to update above code -- both fail
-#   shared_examples_for "all static pages" do
-#     it { should have_selector('h1',text: heading) }
-#     it { should have_selector('title', text: full_title) }
-#   end
-
-#   describe "Home page" do
-#     before { visit root_path }
-#     let(:heading)  { 'New App7' }
-#     let(:page_title) { '' }
-
-#   #  it_should_behave_like "all static pages"
-#   #  it { should_not have_selector 'title', text: '| Home' }
-#   end
-
-#   describe "Help page" do
-#     before { visit help_path }
-#     let(:heading)  { 'New App7' }
-#     let(:page_title) { '' }
-
-#  #   it_should_behave_like "all static pages"
-#   end
-
-#   describe "About page" do
-#     before { visit about_path }
-#     let(:heading)  { 'New App7' }
-#     let(:page_title) { '' }
-
-#   #  it_should_behave_like "all static pages"
-#   end
-
-#   describe "Contact page" do
-#     before { visit contact_path }
-#     let(:heading)  { 'New App7' }
-#     let(:page_title) { '' }
-
-#   #  it_should_behave_like "all static pages"
-#   end
-
-# end
 #####===============================================================
-
-######################################################################
-# old code:
-#     it "should have the h1 'Home'" do
-#      # visit '/static_pages/home'
-#       visit root_path
-#       page.should have_selector('h1', :text => 'Home')
-#     end
-
-#     it "should have the base title" do
-#       # visit '/static_pages/home'
-#       visit root_path
-#       page.should have_selector('title', :text => 'New App7')
-#     end
-
-#   end
-
-#   describe "Contact page" do
-
-#     it "should have the h1 'Contact'" do
-#      # visit '/static_pages/contact'
-#       visit contact_path
-#       page.should have_selector('h1', :text => 'Contact')
-#     end
-
-#     it "should have the base title" do
-#    #   visit '/static_pages/contact'
-#       visit contact_path
-#       page.should have_selector('title', :text => 'Contact')
-#     end
-
-#     # it "should not have a custom page title" do
-#     #   visit contact_path
-#     #   page.should_not have_selector('title', :text => '| Contact')
-#     # end
-#   end
-
-
-#   describe "Help page" do
-
-#     it "should have the h1 'Help'" do
-#     # visit '/static_pages/help'
-#       visit help_path
-#       page.should have_selector('h1', :text => 'Help')
-#     end
-
-#     it "should have the base title" do
-#      # visit '/static_pages/help'
-#      visit help_path
-#       page.should have_selector('title', :text => 'New App7')
-#     end
-
-#     # it "should not have the custom page title" do
-#     #   visit help_path
-#     #   page.should_not have_selector('title', :text => "| Help")
-#     # end
-#   end
-
-#   describe "About page" do
-
-#     it "should have the h1 'About Us'" do
-#     # visit '/static_pages/about'
-#       visit about_path
-#       page.should have_selector('h1', :text => 'About')
-#     end
-
-#     it "should have the base title" do
-#    #   visit '/static_pages/about'
-#       visit about_path
-#       page.should have_selector('title', :text => 'New App7')
-#     end
-
-#     # it "should not have the custom page title" do
-#     #   visit about_path
-#     #   page.should_not have_selector('title', :text => "| About Us")
-#     # end
-#   end
-# end
