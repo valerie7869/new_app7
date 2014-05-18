9fa9059a77726ddb1b21c20e73cb58165de57b36
valerie <valerie.douglas@openlogic.com>
2014-05-18 17:02:48 -0600

    vendor compiled assets

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,46 +1,9 @@
-# require 'spec_helper'
-
-# describe "Static pages" do
-
-#    subject { page }
-
-#   describe "Home page" do
-#     before { visit root_path }
-#     it { should have_selector('h1', text: 'New App7')}
-#     it { should have_selector('title', text: full_title('')) }
-#    # it { should_not have_selector 'title', text: '| Home'}
-#   end   
-
-#   describe "Help page" do
-#     before { visit help_path }
-
-#     it { should have_selector('h1',    text: 'Help') }
-#     it { should have_selector('title', text: full_title('Help')) }
-#   end
-
-#   describe "About page" do
-#     before { visit about_path }
-
-#     it { should have_selector('h1',    text: 'About') }
-#     it { should have_selector('title', text: full_title('About Us')) }
-#   end
-
-#   describe "Contact page" do
-#     before { visit contact_path }
-
-#     it { should have_selector('h1',    text: 'Contact') }
-#     it { should have_selector('title', text: full_title('Contact')) }
-#   end
-# end
-#/////////////////////////////////////////////////////////////////////
-
 require 'spec_helper'
 
 describe "Static pages" do
 
    subject { page }
 
-#below are 2 separate attempts to update above code -- both fail
   shared_examples_for "all static pages" do
     it { should have_selector('h1',     text: heading) }
     it { should have_selector('title',  text: full_title(page_title)) }
